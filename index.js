414a492f6788dfb588cf52a6b3f4030734693aaf
Valentin Iljaž <valentin.iljaz@gmail.com>
2022-03-25 09:22:19 +0100

    Change to CJS

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-import {exec} from "child_process";
+'use strict'
 
-export const tscAlias = () => {
-    return {
-        name: "tscAlias",
-        writeBundle: () => {
-            return new Promise((resolve, reject) => {
-                exec("tsc-alias", function callback(error, stdout, stderr) {
-                    if (stderr || error) {
-                        reject(stderr || error);
-                    } else {
-                        resolve(stdout);
-                    }
-                });
-            });
-        },
-    };
-};
+const cp = require('child_process')
+
+module.exports = function() {
+  return {
+    name: 'tscAlias',
+    writeBundle() {
+      return new Promise((resolve, reject) => {
+        cp.exec("tsc-alias", function callback(error, stdout, stderr) {
+            if (stderr || error) {
+                reject(stderr || error);
+            } else {
+                resolve(stdout);
+            }
+        });
+      });
+    },
+  }
+}
